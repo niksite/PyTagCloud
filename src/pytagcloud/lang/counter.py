4c955925a2df4cd61174e96641b26dceade3a12e
Nikolay A. Panov <nik@laptop.niksite.ru>
2011-06-21 17:44:50 +0400

    About 10x speedup by changing list() to set(), also I see no need in language detection (since texts may be multilanguage) so detection code (which often causes exceptions in present implementation) is removed at all. Overall: significantly improved stability and performance.

--- src/pytagcloud/lang/counter.py
+++ src/pytagcloud/lang/counter.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
 import re
-from pytagcloud.lang.stopwords import StopWords
+from pytagcloud.lang.stopwords import get_stop_words
 from operator import itemgetter
+
 
 def get_tag_counts(text):
     """
-    Search tags in a given text. The language detection is based on stop lists.
-    This implementation is inspired by https://github.com/jdf/cue.language. Thanks Jonathan Feinberg.
+    Search tags in a given text.
     """
-    words = map(lambda x:x.lower(), re.findall(r'\w+', text, re.UNICODE))
-    
-    s = StopWords()     
-    s.load_language(s.guess(words))
-    
+    words = map(lambda x: x.lower(), re.findall(r'[\w-]+', text, re.UNICODE))
+    stop_words = get_stop_words()
     counted = {}
-    
     for word in words:
-        if not s.is_stop_word(word) and len(word) > 1:
-            if counted.has_key(word):
+        if len(word) > 1 and word not in stop_words:
+            if word in counted:
                 counted[word] += 1
-            else: 
+            else:
                 counted[word] = 1
-      
     return sorted(counted.iteritems(), key=itemgetter(1), reverse=True)
-    
